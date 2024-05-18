7cbbdc21715044d727f7e80b92c13763dc85b73c
dangjinghao <dangjinghaoemail@163.com>
2024-05-19 00:06:35 +0800

    modify action order in loop to fit the situation of empty clipboard

--- client/main_loop.go
+++ client/main_loop.go
@@ -15,18 +15,6 @@ func mainLoop(conf *model.Conf, adapter model.ClipboardCmdAdapter, client *http.
 	var previousClipboard model.Clipboard
 	for {
 		time.Sleep(time.Duration(conf.Client.Interval) * time.Millisecond) //sleep first to avoid the possible occured error then it skip sleep
-		currentClipboard, E := adapter.Paste()
-		if E != nil {
-			if E == model.ErrEmptyClipboard {
-				logger.Warnf(`adapter.Paste error:%v ,set empty string clipboard.`, E)
-				currentClipboard = ""
-				// FIXME: it could caused a problem that when I implement pushing local clipboard to server when the first connect.
-			} else {
-				logger.Fatalf("adapter.Paste error:%v", E)
-			}
-
-		}
-		logger.Tracef("adapter.Paste %v", []byte(currentClipboard))
 
 		// It's not a good idea to use PullStringData
 		// because I need the error infomation to skip current look
@@ -47,8 +35,6 @@ func mainLoop(conf *model.Conf, adapter model.ClipboardCmdAdapter, client *http.
 			logger.Fatalf("cannot parse response body: %s", err.Error())
 		}
 
-		previousClipboardHistoryidx := model.IndexClipboardArray(remoteClipboards, &previousClipboard)
-		clipboardContentIfIsFile := deteckAndconcatClipboardFileUrl(conf, &remoteClipboards[0])
 		// Now we just ignore the conflict when all of those are different
 		// Why do we need `previousClipboardHistoryidx`?
 		// Consider the following situation
@@ -60,7 +46,23 @@ func mainLoop(conf *model.Conf, adapter model.ClipboardCmdAdapter, client *http.
 		// text2 will be synchronized to adapter again,
 		// but what we need is a new copied text1
 		// even though text1 is same as previous text1
-		if previousClipboardHistoryidx > 0 {
+
+		previousClipboardHistoryidx := model.IndexClipboardArray(remoteClipboards, &previousClipboard)
+		clipboardContentIfIsFile := deteckAndconcatClipboardFileUrl(conf, &remoteClipboards[0])
+
+		if previousClipboardHistoryidx == -1 {
+			previousClipboard = remoteClipboards[0]
+			logger.Info("This is a new client, synchronizing from server...")
+			E := adapter.Copy(clipboardContentIfIsFile)
+
+			if E != nil {
+				logger.Fatalf("adapter.Copy error:%v", E)
+
+			}
+			logger.Info("synchronize data completed.")
+			logger.Tracef("previousClipboard.Content: %s", previousClipboard.Content)
+			continue
+		} else if previousClipboardHistoryidx > 0 {
 			logger.Infof("Pull <= %v [%v]", remoteClipboards[0].Content, remoteClipboards[0].Hostname)
 			previousClipboard = remoteClipboards[0]
 			E := adapter.Copy(clipboardContentIfIsFile)
@@ -68,10 +70,29 @@ func mainLoop(conf *model.Conf, adapter model.ClipboardCmdAdapter, client *http.
 				logger.Fatalf("adapter.Copy error:%v", E)
 
 			}
+			continue
+		}
+		// else: previousClipboardHistoryidx == 0, detect whether the current clipboard is updated.
+		currentClipboard, E := adapter.Paste()
+		if E != nil {
+			if E == model.ErrEmptyClipboard {
+				logger.Infof(`adapter.Paste error:%v ,set empty string clipboard.`, E)
+				currentClipboard = ""
+			} else {
+				logger.Fatalf("adapter.Paste error:%v", E)
+			}
 
-		} else if previousClipboard.Content != currentClipboard && previousClipboardHistoryidx == 0 {
-			logger.Tracef("previousClipboard.Content is %v\n", []byte(previousClipboard.Content))
-			logger.Tracef("currentClipboard is %v\n", []byte(currentClipboard))
+		}
+		logger.Tracef("adapter.Paste %v", []byte(currentClipboard))
+
+		if currentClipboard == "" {
+			logger.Info("skip current loop because current clipboard is empty")
+			continue
+		}
+
+		logger.Tracef("previousClipboard.Content is %v\n", []byte(previousClipboard.Content))
+		logger.Tracef("currentClipboard is %v\n", []byte(currentClipboard))
+		if previousClipboard.Content != currentClipboard {
 			if currentClipboard == clipboardContentIfIsFile {
 				logger.Debug("currentClipboard is file url clipboard, skip push")
 				continue
@@ -90,22 +111,10 @@ func mainLoop(conf *model.Conf, adapter model.ClipboardCmdAdapter, client *http.
 				logger.Fatalf("push clipboard error: %s", err.Error())
 			}
 			resp.Body.Close()
-
-		} else if previousClipboardHistoryidx == -1 {
-			previousClipboard = remoteClipboards[0]
-			logger.Info("This is a new client, synchronizing from server...")
-			E := adapter.Copy(clipboardContentIfIsFile)
-
-			if E != nil {
-				logger.Fatalf("adapter.Copy error:%v", E)
-
-			}
-			logger.Info("synchronize data completed.")
-			logger.Tracef("previousClipboard.Content: %s", previousClipboard.Content)
-
-		} else {
-			logger.Debug("current clipboard is up-to-date")
+			continue
 		}
+
+		logger.Debug("current clipboard is up-to-date")
 
 	}
 
