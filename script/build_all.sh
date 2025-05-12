19916a97c00f4402b5e7491d587262dad4743302
dangjinghao <dangjinghaoemail@163.com>
2025-05-12 15:17:17 +0800

    FIX: forget shebang

--- script/build_all.sh
+++ script/build_all.sh
@@ -1,11 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "Usage: $0 <build_dir> <name> <ldflags>"
     exit 1
 fi
 
-# "darwin/amd64" "darwin/arm64" are not supported by uclipboard
 GOOSARCHS=("linux/amd64" "linux/arm64" "windows/amd64" "windows/386" "darwin/amd64" "darwin/arm64")
 
 build_dir=$1
@@ -13,7 +12,7 @@ name=$2
 ldflags=$3
 mkdir -p $build_dir
 for osarch in "${GOOSARCHS[@]}"; do
-    IFS=/ read -r GOOS GOARCH <<< "$osarch"
+    IFS='/' read -r GOOS GOARCH <<< "$osarch"
     output_name="${name}-${GOOS}-${GOARCH}"
 
     full_name=$build_dir/$output_name
